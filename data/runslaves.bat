4970b3991017174222df62589eb36e1bd357c895
Carlos Calvo <52244870+ccalvocm@users.noreply.github.com>
2023-05-09 17:37:13 -0400

    add: lateral basins

--- data/runslaves.bat
+++ data/runslaves.bat
@@ -1,2 +1,2 @@
-for /l %%A in (1,1,24) do @xcopy "Choapa_Cuncumen" "Choapa_Cuncumen_%%A" /i
-for /l %%A in (1,1,24) do cd %~dp0Choapa_Cuncumen_%%A & start cmd /k RunSlave.bat
+for /l %%A in (1,1,24) do @xcopy "Cogoti_Embalse_Cogoti" "Cogoti_Embalse_Cogoti_%%A" /i
+for /l %%A in (1,1,24) do cd %~dp0Cogoti_Embalse_Cogoti_%%A & start cmd /k RunSlave.bat
